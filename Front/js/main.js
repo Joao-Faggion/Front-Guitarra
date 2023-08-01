472a3a6a12f423c0ae1d5013812cbf9f98808cbf
Joao-Faggion <joao.faggion@limbersoftware.com.br>
2023-08-01 09:36:31 -0300

    Menu Mobile
    Menu para o mobile feito, so falta as 3 barras fazerem a rotação e se transformarem em um X

--- Front/js/main.js
+++ Front/js/main.js
@@ -1,11 +1,11 @@
 const initApp = () => {
     const hamburgerBtn = document.getElementById('hamburger-button')
-
     const mobileMenu = document.getElementById('mobile-menu')
 
     const toggleMenu = () => {
         mobileMenu.classList.toggle('hidden')
         mobileMenu.classList.toggle('flex')
+        hamburgerBtn.classList.toggle('toggle-btn')
     }
 
     hamburgerBtn.addEventListener('click', toggleMenu)
